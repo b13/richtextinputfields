47fa7d44481053b3d4dde3b75d6c06788156ed22
Achim Fritz <achim.fritz@b13.de>
2023-11-28 10:11:02 +0100

    !!![TASK] define requirements
    * require PHP ^8.1
    * require TYPO3 11 or 12

---
+++ ext_emconf.php
@@ -0,0 +1,17 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'richtextinputfields',
+    'description' => 'Rich text editor for input fields.',
+    'category' => 'backend',
+    'author' => 'b13 GmbH',
+    'author_email' => 'typo3@b13.com',
+    'author_company' => 'b13 GmbH',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => ['typo3' => '11.5.33-12.99.99'],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
