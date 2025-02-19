0b0aa4345e61a4ebe99e7fef99fc915c5b65ea67
Achim Fritz <af@achimfritz.de>
2025-02-19 18:02:42 +0100

    [RELEASE] 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH',
     'state' => 'stable',
-    'version' => '1.1.1',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => ['typo3' => '13.4.5-13.99.99'],
         'conflicts' => [],
